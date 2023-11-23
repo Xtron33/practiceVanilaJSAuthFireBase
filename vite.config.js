afe00fcd5626c4b9880a02b4eef38ec8b992e122
Xtron <xtron_uk@yahoo.com>
2023-11-23 19:52:58 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import liveReload from 'vite-plugin-live-reload'
 
-export default {
-    // ...
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                maincon: resolve(__dirname, 'pages', 'main', 'main.html'),
+                login: resolve(__dirname, 'pages', 'login', 'login.html'),
+                register: resolve(__dirname, 'pages', 'register', 'register.html'),
+            },
+        },
+    },
     plugins: [
         liveReload(['./*.html', './pages/**/*.html'], { alwaysReload: true })
     ]
-}
+})
